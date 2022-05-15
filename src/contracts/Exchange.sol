7641d71e00580070fd078554abc295fe7c1f6700
filipeVenancio <filipe.venancio@fixus.co.ao>
2022-05-15 16:06:47 +0100

    Exchange update

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -103,37 +103,38 @@ contract Exchange is HederaTokenService {
         return tokens[_token][_user];
     }
 
-    function makeOrder(address _tokenGet, int64 _amountGet, address _tokenGive, int64 _amountGive) public {
+    function makeOrder(address _tokenGet, int64 _amountGet, address _tokenGive, int64 _amountGive, address _sender) public {
         orderCount = orderCount.add(1);
-        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
-        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+        orders[orderCount] = _Order(orderCount, _sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+        emit Order(orderCount, _sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
     }
 
-    function cancelOrder(int64 _id) public {
+    function cancelOrder(int64 _id, address _sender) public {
         _Order storage _order = orders[_id];
-        require(address(_order.user) == msg.sender);
+        require(address(_order.user) == _sender);
         require(_order.id == _id); // The order must exist
         orderCancelled[_id] = true;
-        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
+        emit Cancel(_order.id, _sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
     }
 
-    function fillOrder(int64 _id) public {
+    function fillOrder(int64 _id, address _sender) public {
         require(_id > 0 && _id <= orderCount);
         require(!orderFilled[_id]);
         require(!orderCancelled[_id]);
         _Order storage _order = orders[_id];
-        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, _sender);
         orderFilled[_order.id] = true;
     }
 
-    function _trade(int64 _orderId, address _user, address _tokenGet, int64 _amountGet, address _tokenGive, int64 _amountGive) internal {
-        int64 _feeAmount = _amountGive.mul(feePercent).div(100);
-        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+    function _trade(int64 _orderId, address _user, address _tokenGet, int64 _amountGet, address _tokenGive, int64 _amountGive, address _sender) internal {
+        int64 _feeAmount = _amountGive * (feePercent/100);
+        int64 _amountDeduct = _feeAmount + _amountGet;
+        tokens[_tokenGet][_sender] = tokens[_tokenGet][_sender].sub(_amountDeduct);
         tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
         tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
         tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
-        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGet);
-        emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, block.timestamp);
+        tokens[_tokenGive][_sender] = tokens[_tokenGive][_sender].add(_amountGet);
+        emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, _sender, block.timestamp);
     }
 }
 
